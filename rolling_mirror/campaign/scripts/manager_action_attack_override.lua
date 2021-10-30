6ae61ef3db78908ff9818be02469ff384c9ebe0e
Michael Chonewicz <michael.chonewicz@gmail.com>
2021-10-31 01:54:24 +0200

    Added disabling of mirror image when target isnt affected by illusions

--- rolling_mirror/campaign/scripts/manager_action_attack_override.lua
+++ rolling_mirror/campaign/scripts/manager_action_attack_override.lua
@@ -4,8 +4,8 @@ local originalRolls = {};
 
 function onInit()
 	superOnAttack = ActionAttack.onAttack; -- Stash original away
+	ActionAttack.onAttack = onAttack;
 	ActionsManager.registerResultHandler("attack", onAttack);
-
 	ActionsManager.registerResultHandler("mirrorImage", onMirrorImageHit);
 
 	-- Add the effect to the spell
@@ -24,9 +24,8 @@ function onMirrorImageHit(rSource, rTarget, rRoll)
 	local rMessage = ActionsManager.createActionMessage(rSource, rRoll);
 	Comm.deliverChatMessage(rMessage);
 
-	-- Find the original attack roll. Shity FG code doesnt allow for passing arguments to handlers
+	-- Find the original attack roll. Shitty FG code doesnt allow for passing arguments to handlers
 	local originalRoll = originalRolls[rRoll.originalRollId];
-	Debug.console(originalRolls);
 	originalRolls[rRoll.originalRollId] = nil;
 
 	local numMirrorImages = math.min(rRoll.numMirrorImages, 20 - 1); -- max checked outside
@@ -49,7 +48,7 @@ function decrementMirrorImages(rActor, currentNumImages)
 	end
 end
 
--- Based on ActionManager.getDefenseValue
+-- Based on ActionManager.getDefenceValue
 function getMirrorImageDefenseValue(rAttacker, rMirroredTarget, rRoll)
 	-- Base calculations
 	local sAttack = rRoll.sDesc;
@@ -136,7 +135,6 @@ function getMirrorImageDefenseValue(rAttacker, rMirroredTarget, rRoll)
 		nDefenseEffectMod = nBonusSituational;
 	end
 	
-	-- Results
 	return nDefense, 0, nDefenseEffectMod, bADV, bDIS;
 end
 
@@ -249,11 +247,82 @@ end
 
 function onAttack(rSource, rTarget, rRoll)
 	local numMirrorImages = getNumMirrorImages(rTarget);
-	if  numMirrorImages > 0 then
+
+	local reachDistance = reachDistanceBetween(rSource, rTarget);
+
+	-- TODO: Change forum description that this doesnt work with blindness from light
+	-- TODO: Make sure player can see their mirror roll
+	if EffectManager5E.hasEffect(rSource, "Blinded", rTarget) then
+		sendChatMessage(rSource, rTarget, "Attacker is blinded and cannot see mirror images");
+		superOnAttack(rSource, rTarget, rRoll);
+	elseif reachDistance ~= -1 and hasAnyVisionWithinRange({ "blindsight", "truesight" }, reachDistance, rSource) then
+		sendChatMessage(rSource, rTarget, "Attacker can see through the mirror image illusions");
+		superOnAttack(rSource, rTarget, rRoll);
+	elseif numMirrorImages > 0 then
 		local originalRoll = { rSource = rSource, rRoll = rRoll };
 		rollMirrorImageCheck(rTarget, originalRoll, numMirrorImages);
 	else 
 		superOnAttack(rSource, rTarget, rRoll);
+	end
+end
+
+function sendChatMessage(rSource, rTarget, text)
+	local msg = { font = "msgfont", text = text, icon = "portrait_gm_token" };
+	
+	ActionsManager.messageResult(false, rSource, rTarget, msg, msg);
+end
+
+-- Returns whether this actor has any of the vision types named in aVisionTypes within the specified range
+function hasAnyVisionWithinRange(aVisionTypes, nRange, rActor)
+	local nodeType, node = ActorManager.getTypeAndNode(rActor);
+	if node == nil then
+		return false;
+	end
+	local sSenses = DB.getValue(node, "senses", "");
+	local actorVisions = parseVisions(sSenses);
+
+	for _, vision in ipairs(actorVisions) do
+		for _, visionType in ipairs(aVisionTypes) do
+			if vision.name == visionType and vision.distance >= nRange then
+				Debug.console("Has vision within range: ", vision.name);
+				return true;
+			end
+		end
+	end
+
+	Debug.console("No special visions within sight");
+	return false;
+end
+
+function reachDistanceBetween(rSource, rTarget)
+	if rSource == nil or rTarget == nil then
+		return -1;
+	end
+	local srcToken, srcImage = getTokenAndImage(rSource);
+    local tgtToken, tgtImage = getTokenAndImage(rTarget);
+
+	if srcToken == nil or tgtToken == nil then
+		return -1;
+	end
+
+	if srcImage.getDistanceBetween == nil then
+		return -1; --This function is only available in FGU.
+	end
+
+	return srcImage.getDistanceBetween(srcToken, tgtToken);
+end
+
+function getTokenAndImage(rActor)
+	if rActor == nil then
+		return nil, nil;
+	end
+	
+	local token = CombatManager.getTokenFromCT(rActor.sCTNode);
+	local image = ImageManager.getImageControl(token);
+	if image == nil or not image.hasGrid() then
+		return nil, nil;
+	else
+		return token, image;
 	end
 end
 
@@ -284,13 +353,28 @@ function getNumMirrorImages(rActor)
 end
 
 function rollMirrorImageCheck(actor, originalRoll, numImages) 
-	local rRoll = { sType = "mirrorImage", sDesc = "[MIRROR CHECK]", aDice = { "d20" }, nMod = 0 };
+	local rRoll = { sType = "mirrorImage", sDesc = "[MIRROR CHECK]", aDice = { "d20" }, nMod = 0, bTower = false, bSecret = false };
 	rRoll.numMirrorImages = numImages;
 	
 	-- Hacky way of passing the roll info to the mirrorImage handler
 	rRoll.originalRollId = nextFreeId;
 	originalRolls[tostring(nextFreeId)] = originalRoll;
 	nextFreeId = nextFreeId + 1;
-	
+
 	ActionsManager.performAction(nil, actor, rRoll);
+end
+
+function parseVisions(sVisions)
+	local aVisions = {};
+	for s in sVisions:gmatch("([^,]*),?") do
+		local sTrim = StringManager.trim(s);
+		if sTrim ~= "" then
+			local name, value = string.gmatch(sTrim, "(.*)% ([0-9]+).*")();
+			if name ~= "" and value ~= "" and name:lower() ~= "passive perception" then
+				local vision = { name = name:lower(), distance = tonumber(value) };
+				table.insert(aVisions, vision);		
+			end
+		end
+	end
+	return aVisions;
 end
